526255ec0f31b55bf5561e3b8aada040b2dbc0d3
Miki Leskinen <miki.leskinen@reaktor.fi>
2011-12-14 22:03:17 +0200

    First somewhat working version with two users

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
+  $.get('/config', function(config) {
+    _.each(config.users, function(user) {
+      var userRow = '<tr id="' + user.id + '"><td class="pic" style="background: url(\'/img/' + user.id + '.jpg\') no-repeat;"></td><td class="mood" style="background-color: lightGreen;"></td></tr>'
+      $('.users').append(userRow)
+    })
+  })
+
   var socket = io.connect(window.location.href)
-  socket.on('mood', function (data) {
-    console.log(data)
-    $('.main .users .bob').text(data.message)
+  socket.on('mood', function (mood) {
+    $('.users #' + mood.user + ' .mood').text(mood.message)
   })
 })
 
--- team-radar.js
+++ team-radar.js
@@ -1,15 +1,23 @@
 var express = require('express')
 var app = express.createServer()
 var io = require('socket.io').listen(app)
+var fs = require('fs')
+var yaml = require('yaml')
+var configFile = 'config.yaml'
 
 app.use(express.static(__dirname + '/static'))
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendfile(__dirname + '/index.html')
 })
 
-app.get('/mood/:user/:index/:message', function (req, res) {
-  console.log(req.params)
+app.get('/config', function(req, res) {
+  fs.readFile(configFile, function(err, fileContents) {
+    res.send(yaml.eval(fileContents.toString()))
+  })
+})
+
+app.get('/mood/:user/:index/:message', function(req, res) {
   io.sockets.emit('mood', {user: req.params.user, index: req.params.index, message: req.params.message})
   res.send('ok')
 })
