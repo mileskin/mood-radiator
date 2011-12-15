e93b706786138d0a2a0e146fab4bf650b7560fb7
Miki Leskinen <miki.leskinen@reaktor.fi>
2011-12-15 09:32:44 +0200

    Modularize js

---
+++ static/js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  $.teamRadar.init()
+})
+
---
+++ static/js/team-radar.js
@@ -0,0 +1,48 @@
+(function($) {
+  if (!$.teamRadar) {
+    $.teamRadar = {
+      init: init
+    }
+  }
+
+  var moodStyles = {
+    1: 'aboutToDie',
+    2: 'quiteBad',
+    3: 'okish',
+    4: 'quiteHappy',
+    5: 'freakinEcstatic'
+  }
+
+  var defaultMoodMessage = 'business as usual'
+  var defaultMoodStyle = moodStyles[4]
+
+  function init() {
+    initUsers()
+    initMoodUpdateListener()
+  }
+
+  function initUsers() {
+    $.get('/config', function(config) {
+      _.each(config.users, function(user) {
+        var userRow = $('<tr id="' + user.id + '"><td class="pic" style="background: url(\'/img/' + user.id + '.jpg\') no-repeat;"></td><td class="mood">' + defaultMoodMessage + '</td></tr>').addClass(defaultMoodStyle)
+        $('.users').append(userRow)
+      })
+    })
+  }
+
+  function initMoodUpdateListener() {
+    var socket = io.connect(window.location.href)
+    socket.on('moodUpdate', updateMoodForUser)
+  }
+
+  function updateMoodForUser(data) {
+    var moodElem = $('.users #' + data.user + ' .mood')
+    _.each(moodStyles, function(styleClass) {
+      moodElem.removeClass(styleClass)
+    })
+    moodElem
+      .text(data.message)
+      .addClass(moodStyles[data.index])
+  }
+})(jQuery)
+
